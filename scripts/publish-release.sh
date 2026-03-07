8b94ec1a3b76dd8b6fcf37c66d788199e83ab390
keplar-404 <shehabhossen76.services@gmail.com>
2026-03-08 03:29:02 +0600

    chore: bump version to v1.0.1

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -6,19 +6,19 @@ echo "🔔 Publish release version"
 
 # Make sure to run bunx npm version <major|minor|patch> first
 
-echo "==== Start Update Version ===="
+echo "==== Start Update Version ==== "
 
-BEFORE_VERSION=$(cat packages/timeline/package.json | grep version | cut -d '"' -f 4)
-
-bunx --bun npm version apply
-
-AFTER_VERSION=$(cat packages/timeline/package.json | grep version | cut -d '"' -f 4)
-
-echo "==== Version Change Completed, before: $BEFORE_VERSION, after: $AFTER_VERSION ===="
-
+# Update component versions (since they depend on each other, they should have the same version)
+cd packages/engine
+bunx npm version patch
 bun install
-# clean \build
 bun run build
 bun publish --access public
 
-echo "==== ✅ Publish release version completed, version: $AFTER_VERSION ===="
+cd ../timeline
+bunx npm version patch
+bun install
+bun run build
+bun publish --access public
+
+echo "==== ✅ Publish release version completed ===="
